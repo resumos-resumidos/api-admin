4a7c4a811fa927b1f89f263a6ef24e1dc9512ff9
eduardolagolima <eduardolagolima@gmail.com>
2021-03-06 20:10:11 -0300

    feat: [CI/10] - adiciona rotas de autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,40 +1,34 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthJWTController;
 use App\Http\Controllers\DisciplineController;
 use App\Http\Controllers\ContentController;
 use App\Http\Controllers\SummaryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('auth/login', [AuthJWTController::class, 'login']);
+Route::post('auth/register', [AuthJWTController::class, 'register']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::post('auth/logout', [AuthJWTController::class, 'logout']);
+    Route::post('auth/refresh', [AuthJWTController::class, 'refresh']);
+    Route::get('auth/me', [AuthJWTController::class, 'me']);
+
+    Route::get('disciplines', [DisciplineController::class, 'index']);
+    Route::get('disciplines/{discipline}', [DisciplineController::class, 'show']);
+    Route::post('disciplines', [DisciplineController::class, 'store']);
+    Route::put('disciplines/{discipline}', [DisciplineController::class, 'update']);
+    Route::delete('disciplines/{discipline}', [DisciplineController::class, 'destroy']);
+
+    Route::get('contents', [ContentController::class, 'index']);
+    Route::get('contents/{content}', [ContentController::class, 'show']);
+    Route::post('contents', [ContentController::class, 'store']);
+    Route::put('contents/{content}', [ContentController::class, 'update']);
+    Route::delete('contents/{content}', [ContentController::class, 'destroy']);
+
+    Route::get('summaries', [SummaryController::class, 'index']);
+    Route::get('summaries/{summary}', [SummaryController::class, 'show']);
+    Route::post('summaries', [SummaryController::class, 'store']);
+    Route::put('summaries/{summary}', [SummaryController::class, 'update']);
+    Route::delete('summaries/{summary}', [SummaryController::class, 'destroy']);
 });
-
-Route::get('disciplines', [DisciplineController::class, 'index']);
-Route::get('disciplines/{discipline}', [DisciplineController::class, 'show']);
-Route::post('disciplines', [DisciplineController::class, 'store']);
-Route::put('disciplines/{discipline}', [DisciplineController::class, 'update']);
-Route::delete('disciplines/{discipline}', [DisciplineController::class, 'destroy']);
-
-Route::get('contents', [ContentController::class, 'index']);
-Route::get('contents/{content}', [ContentController::class, 'show']);
-Route::post('contents', [ContentController::class, 'store']);
-Route::put('contents/{content}', [ContentController::class, 'update']);
-Route::delete('contents/{content}', [ContentController::class, 'destroy']);
-
-Route::get('summaries', [SummaryController::class, 'index']);
-Route::get('summaries/{summary}', [SummaryController::class, 'show']);
-Route::post('summaries', [SummaryController::class, 'store']);
-Route::put('summaries/{summary}', [SummaryController::class, 'update']);
-Route::delete('summaries/{summary}', [SummaryController::class, 'destroy']);
